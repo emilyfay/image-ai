7b9e7e869459f080081fc471bfba39e96f7b1562
EFay <emilylfay@gmail.com>
2016-09-28 20:51:08 -0700

    poisson blending implemented

--- Image_completer/views.py
+++ Image_completer/views.py
@@ -3,10 +3,12 @@ import random, string
 import shutil
 import subprocess
 import werkzeug
+import gc
 
 import numpy as np
 from flask import render_template, request,redirect,url_for, session
 from scipy import ndimage, misc, sparse
+from PIL import Image
 import pyamg
 
 from Image_completer import app
@@ -157,6 +159,7 @@ def extend():
     shutil.copy(orig_path, raw_path)
 
     session['r_image_file'] = r_filename
+    gc.collect()
 
     return render_template("extend.html", image_path="../static/raw_uploads/"+r_filename)
 
@@ -165,10 +168,10 @@ def extend():
 @app.route('/image-complete')
 def complete_image():
 
-    x1 = session.get('mask_x1')
-    x2 = session.get('mask_x2')
-    y1 = session.get('mask_y1')
-    y2 = session.get('mask_y2')
+    x1 = int(session.get('mask_x1'))
+    x2 = int(session.get('mask_x2'))
+    y1 = int(session.get('mask_y1'))
+    y2 = int(session.get('mask_y2'))
 
     print('here')
     filename = session.get('image_file', False)
@@ -189,18 +192,26 @@ def complete_image():
     print('subprocess')
     subprocess.call(sub_script, shell = True)
 
-
     big_not_mask = np.ones([x_,y_,3])
-
     big_not_mask[x1:x2, y1:y2,:] = 0.0
-
     big_mask = 1-big_not_mask
 
-    filled_image = ndimage.imread(filled_path, mode='RGB').astype(float)
+    blended_path = filled_folder+"b"+r_filename
+    target = np.asarray(Image.open(origin_path).resize((64,64),resample=Image.LANCZOS))
+    source = np.asarray(Image.open(filled_path))
+    mask = np.zeros((64,64))
+    mask[x1:x2,y1:y2] = 1.0
 
-    big_filled = misc.imresize(filled_image,(x_,y_,3))
+    mask.flags.writeable = True
+    target.flags.writeable = True
+    source.flags.writeable = True
 
-    complete_im = np.add(np.multiply(original_im, big_not_mask),np.multiply(big_filled,big_mask))
+    small_blended = blend(target,source,mask,offset=(0,0))
+    misc.imsave(blended_path,small_blended)
+    big_blended = misc.imresize(small_blended,(x_,y_,3))
+    big_blended = big_blended[:,:,:3]
+
+    complete_im = np.add(np.multiply(original_im, big_not_mask),np.multiply(big_blended,big_mask))
     completed_path = completed_folder+r_filename
     misc.imsave(completed_path, complete_im)
     return render_template("display_completed_image.html", ds_image_path = "../static/masked_images/"+r_filename,image_path="../static/completed_images/"+r_filename)
@@ -229,7 +240,7 @@ def image_extend():
 
     if edge == "T":
         # extend the top
-        image_holder = np.random.normal(size=[x_+ex_x,y_,3], scale = 1)
+        image_holder = np.random.normal(size=[x_+ex_x,y_,3], scale = 0.01)
         big_not_mask = np.zeros([x_+ex_x,y_,3])
         image_holder[ex_x:,:,:] = in_im
         big_not_mask[ex_x+1:,:,:] = 1.0
@@ -241,7 +252,7 @@ def image_extend():
         y_f = y_
     elif edge == "L":
         # extend left edge
-        image_holder = np.random.normal(size=[x_,y_+ex_y,3], scale = 1)
+        image_holder = np.random.normal(size=[x_,y_+ex_y,3], scale = 0.01)
         image_holder[:,ex_y:,:] = in_im
         big_not_mask = np.zeros([x_,y_+ex_y,3])
         big_not_mask[:,ex_y+1:,:] = 1.0
@@ -253,7 +264,7 @@ def image_extend():
         y_f = y_+ex_y
     elif edge == "R":
         # extend right edge
-        image_holder = np.random.normal(size=[x_,y_+ex_y,3], scale = 1)
+        image_holder = np.random.normal(size=[x_,y_+ex_y,3], scale = 0.01)
         image_holder[:,:y_,:] = in_im
         big_not_mask = np.zeros([x_,y_+ex_y,3])
         big_not_mask[:,:y_-1,:] = 1.0
@@ -265,7 +276,7 @@ def image_extend():
         y_f = y_+ex_y
     else:
         # extend the bottom
-        image_holder = np.random.normal(size=[x_+ex_x,y_,3], scale = 1)
+        image_holder = np.random.normal(size=[x_+ex_x,y_,3], scale = 0.01)
         image_holder[:x_,:,:] = in_im
         big_not_mask = np.zeros([x_+ex_x,y_,3])
         big_not_mask[:x_-1,:,:] = 1.0
@@ -279,24 +290,34 @@ def image_extend():
     origin_path = masked_folder+r_filename
     misc.imsave(origin_path, image_holder)
 
-
     filled_path = filled_folder+"AI"+r_filename
     sub_script = "python /home/ubuntu/web_app_AWS/image_fill_deconv.py --image_path %s --out_path %s --mask_x1 %i --mask_x2 %i --mask_y1 %i --mask_y2 %i" %(origin_path,filled_path,x1,x2,y1,y2)
     print('subprocess')
     subprocess.call(sub_script, shell = True)
 
     big_mask = 1-big_not_mask
-    rs_filled_path = filled_folder+"rsAI"+r_filename
-    filled_image = ndimage.imread(filled_path, mode='RGB').astype(float)
 
-    big_filled = misc.imresize(filled_image,(x_f,y_f,3))
-    misc.imsave(rs_filled_path, big_filled)
     masked_im = np.multiply(image_holder, big_not_mask)
     masked_path = masked_folder+"M"+r_filename
     misc.imsave(masked_path, masked_im)
-    complete_im = np.add(np.multiply(image_holder, big_not_mask),np.multiply(big_filled,big_mask))
+
+    blended_path = filled_folder+"b"+r_filename
+    target = np.asarray(Image.open(masked_path).resize((64,64),resample=Image.LANCZOS))
+    source = np.asarray(Image.open(filled_path))
+    mask = np.zeros((64,64))
+    mask[x1:x2,y1:y2] = 1.0
+
+    mask.flags.writeable = True
+    target.flags.writeable = True
+    source.flags.writeable = True
+
+    small_blended = blend(target,source,mask,offset=(0,0))
+    misc.imsave(blended_path,small_blended)
+    big_blended = misc.imresize(small_blended,(x_f,y_f,3))
+    complete_im = np.add(np.multiply(image_holder, big_not_mask),np.multiply(big_blended,big_mask))
     completed_path = completed_folder+r_filename
     misc.imsave(completed_path, complete_im)
+
     return render_template("display_completed_image.html", ds_image_path="../static/masked_images/"+"M"+r_filename,image_path="../static/completed_images/"+r_filename)
 
 
@@ -307,26 +328,27 @@ app.secret_key = os.urandom(24)
 def blend(img_target, img_source, img_mask, offset=(0, 0)):
     # compute regions to be blended
     region_source = (
-            max(-offset[0], 0),
-            max(-offset[1], 0),
-            min(img_target.shape[0]-offset[0], img_source.shape[0]),
-            min(img_target.shape[1]-offset[1], img_source.shape[1]))
+            0,
+            0,
+            64,
+            64)
     region_target = (
-            max(offset[0], 0),
-            max(offset[1], 0),
-            min(img_target.shape[0], img_source.shape[0]+offset[0]),
-            min(img_target.shape[1], img_source.shape[1]+offset[1]))
-    region_size = (region_source[2]-region_source[0], region_source[3]-region_source[1])
+            0,
+            0,
+            64,
+            64)
+    region_size = (64,64)#region_source[2]-region_source[0], region_source[3]-region_source[1])
 
     # clip and normalize mask image
-    img_mask = img_mask[region_source[0]:region_source[2], region_source[1]:region_source[3]]
+    #img_mask = img_mask[region_source[0]:region_source[2], region_source[1]:region_source[3]]
     img_mask[img_mask==0] = False
     img_mask[img_mask!=False] = True
 
     # create coefficient matrix
     A = sparse.identity(np.prod(region_size), format='lil')
-    for y in range(region_size[0]):
-        for x in range(region_size[1]):
+
+    for y in range(64):
+        for x in range(64):
             if img_mask[y,x]:
                 index = x+y*region_size[1]
                 A[index, index] = 4
@@ -344,10 +366,10 @@ def blend(img_target, img_source, img_mask, offset=(0, 0)):
     P = pyamg.gallery.poisson(img_mask.shape)
 
     # for each layer (ex. RGB)
-    for num_layer in range(img_target.shape[2]):
+    for num_layer in range(3):
         # get subimages
-        t = img_target[region_target[0]:region_target[2],region_target[1]:region_target[3],num_layer]
-        s = img_source[region_source[0]:region_source[2], region_source[1]:region_source[3],num_layer]
+        t = img_target[:,:,num_layer]
+        s = img_source[:, : ,num_layer]
         t = t.flatten()
         s = s.flatten()
 
@@ -358,7 +380,7 @@ def blend(img_target, img_source, img_mask, offset=(0, 0)):
                 if not img_mask[y,x]:
                     index = x+y*region_size[1]
                     b[index] = t[index]
-
+        print('here')
         # solve Ax = b
         x = pyamg.solve(A,b,verb=False,tol=1e-10)
 
