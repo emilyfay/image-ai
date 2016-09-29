793a9be13a46224c8f4bb4382e4d21fac12a9e00
EFay <emilylfay@gmail.com>
2016-09-28 22:18:53 -0700

    update html for image scaling

--- Image_completer/views.py
+++ Image_completer/views.py
@@ -127,9 +127,9 @@ def apply_mask():
     szN = 2*np.floor(im_x_size/64)
 
     x1 = im_x_size-np.floor(im_x_size/4)
-    x2 = im_x_size-szN
-    y1 = np.floor(im_y_size/3.5)
-    y2 = np.floor(im_y_size/2)
+    x2 = im_x_size-szN+2
+    y1 = np.floor(im_y_size/5.5)
+    y2 = np.floor(im_y_size/2.8)
 
     mask[x1:x2,y1:y2,:] = noise_mat[x1:x2,y1:y2,:]
     not_mask[x1:x2,y1:y2,:] = 0.0
@@ -302,72 +302,3 @@ def image_extend():
 
 # set the secret key.
 app.secret_key = os.urandom(24)
-
-# function to apply poisson blending, from github.com/parosky
-def blend(img_target, img_source, img_mask, offset=(0, 0)):
-    # compute regions to be blended
-    region_source = (
-            max(-offset[0], 0),
-            max(-offset[1], 0),
-            min(img_target.shape[0]-offset[0], img_source.shape[0]),
-            min(img_target.shape[1]-offset[1], img_source.shape[1]))
-    region_target = (
-            max(offset[0], 0),
-            max(offset[1], 0),
-            min(img_target.shape[0], img_source.shape[0]+offset[0]),
-            min(img_target.shape[1], img_source.shape[1]+offset[1]))
-    region_size = (region_source[2]-region_source[0], region_source[3]-region_source[1])
-
-    # clip and normalize mask image
-    img_mask = img_mask[region_source[0]:region_source[2], region_source[1]:region_source[3]]
-    img_mask[img_mask==0] = False
-    img_mask[img_mask!=False] = True
-
-    # create coefficient matrix
-    A = sparse.identity(np.prod(region_size), format='lil')
-    for y in range(region_size[0]):
-        for x in range(region_size[1]):
-            if img_mask[y,x]:
-                index = x+y*region_size[1]
-                A[index, index] = 4
-                if index+1 < np.prod(region_size):
-                    A[index, index+1] = -1
-                if index-1 >= 0:
-                    A[index, index-1] = -1
-                if index+region_size[1] < np.prod(region_size):
-                    A[index, index+region_size[1]] = -1
-                if index-region_size[1] >= 0:
-                    A[index, index-region_size[1]] = -1
-    A = A.tocsr()
-
-    # create poisson matrix for b
-    P = pyamg.gallery.poisson(img_mask.shape)
-
-    # for each layer (ex. RGB)
-    for num_layer in range(img_target.shape[2]):
-        # get subimages
-        t = img_target[region_target[0]:region_target[2],region_target[1]:region_target[3],num_layer]
-        s = img_source[region_source[0]:region_source[2], region_source[1]:region_source[3],num_layer]
-        t = t.flatten()
-        s = s.flatten()
-
-        # create b
-        b = P * s
-        for y in range(region_size[0]):
-            for x in range(region_size[1]):
-                if not img_mask[y,x]:
-                    index = x+y*region_size[1]
-                    b[index] = t[index]
-
-        # solve Ax = b
-        x = pyamg.solve(A,b,verb=False,tol=1e-10)
-
-        # assign x to target image
-        x = np.reshape(x, region_size)
-        x[x>255] = 255
-        x[x<0] = 0
-        x = np.array(x, img_target.dtype)
-        img_target[region_target[0]:region_target[2],region_target[1]:region_target[3],num_layer] = x
-
-    return img_target
-
